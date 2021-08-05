48866a114ddb7dc9cccdc2661fc0f16616aa199f
Alroy Fernandes <alroyfernandes07518@gmail.com>
2021-08-05 18:40:37 +0530

    vaz

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from "react";
-import Navbar from "./components/Navbar/Navbar";
+
 import { BrowserRouter as Router,Switch,Route } from "react-router-dom";
 import "./App.css";
-import SideBar from "./components/SideBar/SideBar";
+
 import Home from "./pages/Home";
 import Signin from "./pages/Signin";
 
